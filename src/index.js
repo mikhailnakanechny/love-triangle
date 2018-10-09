3c1bbedcbef0746686a78ee0d65f58bb0aacda50
mikhailnakanechny <mikhailnakanechny@gmail.com>
2018-10-09 22:17:30 +0300

    ref: refactored to ES6

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- *
- */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-    // your implementation
-    var counter = 0;
+    let counter = 0;
 
-    for (var i = 0; i < preferences.length; i++) {
-        //узнаем значения по ссылкам
-        var x1 = preferences[i];
-        var x2 = preferences[x1 - 1];
-        var x3 = preferences[x2 - 1];
-        //  проверяем соответствие условиям: последний элемент возвращает номер первого
-        if((x3-1===i)){
-            counter++;
-        }
+    for (let i = 0; i < preferences.length; i++) {
+        const x1 = preferences[i],
+              x2 = preferences[x1 - 1],
+              x3 = preferences[x2 - 1];
+        if((x3-1===i)){counter++;}
     }
 
     return Math.floor(counter/3);
